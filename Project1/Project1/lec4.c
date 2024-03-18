4df9da8906b851e01c8541c9952ab64664752182
lejw-jbnu <zealot1998@naver.com>
2024-03-18 11:38:19 +0900

    최대값

---
+++ Project1/Project1/lec4.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int T, test_case;
+
+		freopen("input.txt", "r", stdin);
+		setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (test_case = 1; test_case <= T; ++test_case) {
+		int num[10];
+		for (int i = 0; i < 10; i++) {
+			scanf("%d", &num[i]);
+		}
+
+		int m = 0;
+		for (int i = 0; i < 10; i++) {
+			if (m < num[i]) {
+				m = num[i];
+			}
+		}
+
+		printf("#%d %d\n",test_case, m);
+	}
+}
